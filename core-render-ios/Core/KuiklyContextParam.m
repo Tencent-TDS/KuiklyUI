0b4076c6208bc548d88589480a5aa6ed956a220a
HongShaojie <hongshaojie@taqu.cn>
2025-08-20 19:47:23 +0800

    feat：iOS支持通过文件名从自定义bundle获取图片等资源URL，适用于自定义资源bundle（非mainBundle的场景）

--- core-render-ios/Core/KuiklyContextParam.m
+++ core-render-ios/Core/KuiklyContextParam.m
@@ -15,6 +15,7 @@
 
 #import "KuiklyContextParam.h"
 #import "KuiklyRenderFrameworkContextHandler.h"
+#import "KuiklyRenderBridge.h"
 
 const KuiklyContextMode KuiklyContextMode_Framework = 1;
 
@@ -58,7 +59,13 @@ const KuiklyContextMode KuiklyContextMode_Framework = 1;
 }
 
 - (NSURL *)urlForFileName:(NSString *)fileName extension:(NSString *)fileExtension {
-    if (self.resourceFolderUrl) {
+    // 通过fileName从自定义bundle中获取资源URL
+    if ([[KuiklyRenderBridge componentExpandHandler] respondsToSelector:@selector(hr_customBundleUrlForFileName:extension:)]) {
+        return [[KuiklyRenderBridge componentExpandHandler] hr_customBundleUrlForFileName:fileName
+                                                                                extension:fileExtension];
+    }
+    // 通过resourceFolderUrl从自定义bundle中获取资源URL
+    else if (self.resourceFolderUrl) {
         NSURL *rscUrl = [[self.resourceFolderUrl URLByAppendingPathComponent:fileName] URLByAppendingPathExtension:fileExtension];
         return rscUrl;
     }

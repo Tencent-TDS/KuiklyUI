40c756acbd4b5b21f0b1e8c3f4d3291d91196700
FeanCheng <2111642311@qq.com>
2025-09-08 10:49:43 +0800

    fix:fix listView vforlazy update data view no add

--- demo/src/commonMain/kotlin/com/tencent/kuikly/demo/pages/demo/kit_demo/DeclarativeDemo/LazyListExamplePage.kt
+++ demo/src/commonMain/kotlin/com/tencent/kuikly/demo/pages/demo/kit_demo/DeclarativeDemo/LazyListExamplePage.kt
@@ -1,7 +1,7 @@
 /*
  * Tencent is pleased to support the open source community by making KuiklyUI
  * available.
- * Copyright (C) 2025 THL A29 Limited, a Tencent company. All rights reserved.
+ * Copyright (C) 2025 Tencent. All rights reserved.
  * Licensed under the License of KuiklyUI;
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 
 package com.tencent.kuikly.demo.pages.demo.kit_demo.DeclarativeDemo
 

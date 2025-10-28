1f0ec02159e46f96b7ccec925959f4dbc9acde0d
elixxli <elixxli@tencent.com>
2025-10-28 17:19:59 +0800

    chore: modify some detail

--- build.2.0.21.gradle.kts
+++ build.2.0.21.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
     kotlin("multiplatform") version "2.0.21" apply false
-//    kotlin("plugin.compose") version "2.1.21" apply false
     id("com.android.application") version "7.4.2" apply false
     id("com.android.library") version "7.4.2" apply false
-//    id("org.jetbrains.compose") version "1.7.3" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
 
@@ -17,7 +15,6 @@ buildscript {
     dependencies {
         classpath(BuildPlugin.kotlin)
         classpath(BuildPlugin.android)
-        classpath(BuildPlugin.kuikly)
     }
 }
 
@@ -31,7 +28,6 @@ allprojects {
         resolutionStrategy.dependencySubstitution {
             substitute(module("${MavenConfig.GROUP}:core")).using(project(":core"))
             substitute(module("${MavenConfig.GROUP}:core-annotations")).using(project(":core-annotations"))
-//            substitute(module("${MavenConfig.GROUP}:compose")).using(project(":compose"))
         }
     }
 }

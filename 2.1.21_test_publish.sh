b964d89a2068e6b98e3cd36ff364dd9a3f9890d4
elixxli <elixxli@tencent.com>
2025-10-28 20:14:52 +0800

    chore: modify some detail

--- 2.1.21_test_publish.sh
+++ 2.1.21_test_publish.sh
@@ -6,14 +6,14 @@ NEW_DISTRIBUTION_URL="https\:\/\/services.gradle.org\/distributions\/gradle-7.6.
 sed -i.bak "s/distributionUrl=.*$/distributionUrl=$NEW_DISTRIBUTION_URL/" gradle/wrapper/gradle-wrapper.properties
 
 # 3.开始发布
-#KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-annotations:publishToMavenLocal --stacktrace
-#KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core:publishToMavenLocal --stacktrace
-#KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-ksp:publishToMavenLocal --stacktrace
-#KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-render-android:publishToMavenLocal --stacktrace
+KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-annotations:publishToMavenLocal --stacktrace
+KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core:publishToMavenLocal --stacktrace
+KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-ksp:publishToMavenLocal --stacktrace
+KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-render-android:publishToMavenLocal --stacktrace
 KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :compose:publishToMavenLocal --stacktrace
-#KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-render-web:base:publishToMavenLocal --stacktrace
-#KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-render-web:h5:publishToMavenLocal --stacktrace
-#KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-render-web:miniapp:publishToMavenLocal --stacktrace
+KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-render-web:base:publishToMavenLocal --stacktrace
+KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-render-web:h5:publishToMavenLocal --stacktrace
+KUIKLY_AGP_VERSION="7.4.2" KUIKLY_KOTLIN_VERSION="2.1.21" ./gradlew -c settings.2.1.21.gradle.kts :core-render-web:miniapp:publishToMavenLocal --stacktrace
 
 # 4.还原文件
 mv gradle/wrapper/gradle-wrapper.properties.bak gradle/wrapper/gradle-wrapper.properties

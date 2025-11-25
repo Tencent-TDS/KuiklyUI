822f755d4b1efaa3be41ac941ea69af24d6a8533
zealotchen <zealotchen@tencent.com>
2025-11-25 15:47:03 +0800

    feat: fix lint

--- h5App/src/jsMain/kotlin/processor/CustomImageProcessor.kt
+++ h5App/src/jsMain/kotlin/processor/CustomImageProcessor.kt
@@ -6,13 +6,11 @@ import org.w3c.dom.HTMLImageElement
 
 /**
  * Custom image processor for CDN assets
- * 自定义图片处理器，将 assets 资源指向 CDN
  */
 object CustomImageProcessor : IImageProcessor {
     // Assets image resource prefix, identifies assets resource images
     private const val ASSETS_IMAGE_PREFIX = "assets://"
     // CDN base URL for static assets
-    // 静态资源 CDN 地址，可根据环境配置修改
     private const val CDN_BASE_URL = "https://custom.com/assets/"
 
     override fun getImageAssetsSource(src: String): String {

8338981f6590020645c2e8e83c79c8b914dda4c5
xubin <67296761+RoeKun@users.noreply.github.com>
2025-12-16 23:00:52 +0800

    fix: ios abnormal image load between KRImageViews

--- iosApp/iosApp/KuiklyRenderExpand/Handlers/KuiklyRenderComponentExpandHandler.m
+++ iosApp/iosApp/KuiklyRenderExpand/Handlers/KuiklyRenderComponentExpandHandler.m
@@ -32,6 +32,57 @@
     return nil;
 }
 
+
+/*
+ * 自定义图片src一致性判断
+ * @param src 设置的src属性
+ * @param imageURL 资源的路径
+ * @return 是否满足一致性判断的条件
+ */
+- (BOOL)hr_srcMatch:(NSString *)src imageURL:(NSURL *)imageURL {
+    if (!src.length || !imageURL)
+        return NO;
+    
+    NSString *url = imageURL.absoluteString;
+    if (!url.length)
+        return NO;
+    
+    // 网络URL 走完全匹配
+    if ([url isEqualToString:src])
+        return YES;
+    
+    // 本地资源 取src和url 最后一个“/"之后的内容
+    NSString *srcFileName = [self p_fileNameFromPath:src];
+    NSString *urlFileName = [self p_fileNameFromPath:url];
+    return srcFileName.length && urlFileName.length && [srcFileName isEqualToString:urlFileName];
+}
+
+/**
+ * 提取文件名（去除?和#参数，提取最后一个/之后的文件名称）
+ */
+- (NSString *)p_fileNameFromPath:(NSString *)path {
+    if (!path.length)
+        return @"";
+    
+    // 去除 URL 参数和锚点
+    NSRange range = [path rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"?#"]];
+    if (range.location != NSNotFound) {
+        path = [path substringToIndex:range.location];
+    }
+    
+    // 提取最后一个 / 之后的内容
+    NSRange slashRange = [path rangeOfString:@"/" options:NSBackwardsSearch];
+    if (slashRange.location != NSNotFound) {
+        // 如果 / 是最后一个字符，返回 ""
+        if (slashRange.location == path.length - 1) {
+            return @"";
+        }
+        return [path substringFromIndex:slashRange.location + 1];
+    }
+    return path;
+}
+
+
 /*
  * 自定义实现设置图片
  * @param url 设置的图片url，如果url为nil，则是取消图片设置，需要view.image = nil
@@ -62,5 +113,28 @@
     }];
     return YES;
 }
+
+
+/*
+ * 自定义实现设置图片（带完成回调和src一致性验证，优先调用该方法）
+ * @param url 设置的图片url，如果url为nil，则是取消图片设置，需要view.image = nil
+ * @param placeholder 设置的占位图，默认设置为nil
+ * @param options SDWebImage的图片加载参数，默认为SDWebImageAvoidAutoSetImage，阻断SDWebImage无感更新ImageView的image
+ * @param complete 图片处理完成后的回调，内置src一致性验证
+ * @return 是否处理该图片设置，返回值为YES，则交给该代理实现，否则sdk内部自己处理
+ */
+- (BOOL)hr_setImageWithUrl:(nonnull NSString *)url forImageView:(nonnull UIImageView *)imageView placeholderImage:(nullable UIImage *)placeholder options:(SDWebImageOptions)options complete:(ImageCompletionBlock)completeBlock {
+    [imageView sd_setImageWithURL:[NSURL URLWithString:url]
+                 placeholderImage:placeholder
+                          options:SDWebImageAvoidAutoSetImage
+                        completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+        if (completeBlock) {
+            completeBlock(image, error, [NSURL URLWithString:url]);
+        }
+    }];
+    return YES;
+}
+
+
     
 @end

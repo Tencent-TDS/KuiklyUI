31234d5c04428f80cddd77644ab599c2c8bf55a3
xubin <67296761+RoeKun@users.noreply.github.com>
2026-02-06 10:37:21 +0800

    fix: ComposeDSL Color abnormal issue add comment

--- compose/src/commonMain/kotlin/com/tencent/kuikly/compose/foundation/text/BasicText.kt
+++ compose/src/commonMain/kotlin/com/tencent/kuikly/compose/foundation/text/BasicText.kt
@@ -70,6 +70,7 @@ import com.tencent.kuikly.compose.ui.unit.isSpecified
 import com.tencent.kuikly.compose.ui.util.fastRoundToInt
 import com.tencent.kuikly.compose.extension.scaleToDensity
 import com.tencent.kuikly.compose.ui.platform.LocalConfiguration
+import com.tencent.kuikly.core.log.KLog
 import com.tencent.kuikly.core.views.ISpan
 import com.tencent.kuikly.core.views.PlaceholderSpan
 import com.tencent.kuikly.core.views.RichTextAttr
@@ -568,10 +569,12 @@ private fun TextAttr.applyFontFamily(fontFamily: FontFamily?) {
 }
 
 private fun TextAttr.applyShadow(shadow: Shadow?) {
-    if (shadow == null || (shadow.offset == Offset.Zero && shadow.blurRadius == 0f)) {
-        // 当shadow为空时，重置阴影设置
+    if (shadow == null || (shadow.offset == Offset.Zero && shadow.blurRadius <= 0) ) {
+        // 当 shadow 为空时， 设置空白 shadow
+        // 当 shadow 的offset和Radius全部为零时，shadow无意义，则创建透明shadow
         textShadow(0f, 0f, 0f, Color.Transparent.toKuiklyColor())
     } else if (shadow.color != Color.Unspecified || shadow.offset != Offset.Zero || shadow.blurRadius > 0) {
+        // shadow color 默认的颜色是 黑色，这在iOS侧会引起颜色黯淡
         textShadow(
             offsetX = shadow.offset.x,
             offsetY = shadow.offset.y,

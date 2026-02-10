9c80940bd27b4fd3b5f08890196f889c8fbbd985
elixxli <elixxli@tencent.com>
2026-02-10 12:09:38 +0800

    feat: Android safeArea bottom

---
+++ demo/src/commonMain/kotlin/com/tencent/kuikly/demo/pages/demo/SafeAreaBottomPage.kt
@@ -0,0 +1,39 @@
+package com.tencent.kuikly.demo.pages.demo
+
+import com.tencent.kuikly.demo.pages.base.BasePager
+import com.tencent.kuikly.core.annotations.Page
+import com.tencent.kuikly.core.base.Color
+import com.tencent.kuikly.core.base.ViewBuilder
+import com.tencent.kuikly.core.layout.FlexDirection
+import com.tencent.kuikly.core.views.Text
+import com.tencent.kuikly.core.views.View
+
+@Page("SafeAreaBottomPage")
+internal class SafeAreaBottomPage : BasePager() {
+    
+    override fun body(): ViewBuilder {
+        val ctx = this
+        return {
+            attr {
+                flex(1f)
+                flexDirection(FlexDirection.COLUMN_REVERSE)
+                backgroundColor(Color.YELLOW)
+            }
+            View {
+                attr {
+                    allCenter()
+                    backgroundColor(Color.RED)
+                    ctx.pageData.navigationBarHeight
+                    marginBottom(ctx.pageData.safeAreaInsets.bottom)
+                }
+                Text {
+                    attr {
+                        fontSize(20f)
+                        textAlignCenter()
+                        text("底部导航栏高度测试")
+                    }
+                }
+            }
+        }
+    }
+}

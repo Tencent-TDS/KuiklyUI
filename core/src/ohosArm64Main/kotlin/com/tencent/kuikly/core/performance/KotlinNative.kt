dfc59d909ef78f091b9e5b504a5b3ef5fec86b5a
elixxli <elixxli@tencent.com>
2026-02-24 10:52:58 +0800

    chore: delete some log

--- core/src/ohosArm64Main/kotlin/com/tencent/kuikly/core/performance/KotlinNative.kt
+++ core/src/ohosArm64Main/kotlin/com/tencent/kuikly/core/performance/KotlinNative.kt
@@ -9,9 +9,7 @@ fun getNativeHeapSize(): Long {
     val nativeHeapSize = if (memoryMap != null) {
         var size : Long = 0
         for (entry in memoryMap) {
-            println("xxx key = ${entry.key}")
             val bytes = entry.value.totalObjectsSizeBytes
-            println("xxx value = $bytes")
             size += entry.value.totalObjectsSizeBytes
         }
         size

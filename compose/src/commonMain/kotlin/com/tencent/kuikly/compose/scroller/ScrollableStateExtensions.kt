8215074133559cf6b2bbeb80b4e9034adaa4d56c
zenipchen <870658410@qq.com>
2026-03-05 11:06:43 +0800

    feat(compose): add scrollToTop support for Compose scrollable components (#1090)

--- compose/src/commonMain/kotlin/com/tencent/kuikly/compose/scroller/ScrollableStateExtensions.kt
+++ compose/src/commonMain/kotlin/com/tencent/kuikly/compose/scroller/ScrollableStateExtensions.kt
@@ -123,6 +123,7 @@ internal suspend fun ScrollableState.animateScrollToTop() {
         is LazyListState -> this.animateScrollToItem(0)
         is LazyGridState -> this.animateScrollToItem(0)
         is LazyStaggeredGridState -> this.animateScrollToItem(0)
+        is PagerState -> this.animateScrollToPage(0)
         else -> {}
     }
 }
@@ -150,7 +151,22 @@ internal fun ScrollableState.requestScrollToTop() {
         is LazyListState -> requestScrollToItem(0)
         is LazyGridState -> requestScrollToItem(0)
         is LazyStaggeredGridState -> requestScrollToItem(0)
+        is PagerState -> requestScrollToPage(0)
         // ScrollState does not have request API; skip for now
         else -> {}
+    }
+}
+
+/**
+ * Check if scroll to top callback is set.
+ * If callback is set, invoke it and return true; otherwise return false.
+ */
+internal fun ScrollableState.handleScrollToTopCallback(): Boolean {
+    val callback = kuiklyInfo.scrollToTopCallback
+    return if (callback != null) {
+        callback.invoke()
+        true
+    } else {
+        false
     }
 }

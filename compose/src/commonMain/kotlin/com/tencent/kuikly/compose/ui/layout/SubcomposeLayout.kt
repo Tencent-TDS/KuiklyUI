0e4f54cf0a900b59c1f24d65da0aedf4d758b142
jonasluo <jonasluo@tencent.com>
2026-03-05 17:32:05 +0800

    refactor: migrate render properties from extProps map to dedicated RenderProperties class
    - Add RenderProperties.kt with typed fields for matrix, alpha, measuredSize, borderRadius, clip, clipPath, shadowElevation, shadowColor, shadowHasSet, kuiklyScrollInfo
    - Migrate KNode.kt property access from extPropsVar delegate to RenderProperties fields
    - Update SubcomposeLayout.kt to use obtainRenderProps() for kuiklyScrollInfo
    - Remove unused extPropsVar() factory function from ExtPropsValue.kt

--- compose/src/commonMain/kotlin/com/tencent/kuikly/compose/ui/layout/SubcomposeLayout.kt
+++ compose/src/commonMain/kotlin/com/tencent/kuikly/compose/ui/layout/SubcomposeLayout.kt
@@ -75,6 +75,7 @@ import com.tencent.kuikly.compose.scroller.kuiklyWillDragEnd
 import com.tencent.kuikly.compose.scroller.requestScrollToTop
 import com.tencent.kuikly.compose.scroller.tryExpandStartSize
 import com.tencent.kuikly.compose.ui.node.ComposeUiNode.Companion.ShadowLayoutConstructor
+import com.tencent.kuikly.compose.ui.node.KNode.Companion.obtainRenderProps
 import com.tencent.kuikly.compose.ui.scaleWithDensity
 import com.tencent.kuikly.core.base.DeclarativeBaseView
 import com.tencent.kuikly.core.base.event.layoutFrameDidChange
@@ -234,6 +235,10 @@ fun SubcomposeLayout(
             return@LaunchedEffect
         }
 
+        scrollableState.kuiklyInfo.scrollView = scrollViewRef
+        scrollableState.kuiklyInfo.orientation = orientation
+        val rp = scrollViewRef?.obtainRenderProps()
+        rp?.kuiklyScrollInfo = scrollableState.kuiklyInfo
         val kuiklyInfo = scrollableState.kuiklyInfo
 
         // Bind the new scrollView after computing reset flags so density is available for reset
@@ -373,7 +378,8 @@ fun SubcomposeLayout(
                 scrollViewRef = this.view as? ScrollerView<ScrollerAttr, ScrollerEvent>
                 scrollableState.kuiklyInfo.scrollView = scrollViewRef
                 scrollableState.kuiklyInfo.orientation = orientation
-                scrollViewRef?.extProps?.set(KuiklyInfoKey, scrollableState.kuiklyInfo as Any)
+                val rp = scrollViewRef?.obtainRenderProps()
+                rp?.kuiklyScrollInfo = scrollableState.kuiklyInfo
                 scrollViewSize =
                     Size(
                         width = scrollViewRef?.renderView?.currentFrame?.width ?: 0f,

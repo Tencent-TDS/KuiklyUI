4b2faf22a86e1a036cd62fef8e89b4f51824cd62
etkmao <etkmao@tencent.com>
2026-06-01 16:55:33 +0800

    fix(miniApp): fix custom font demo

--- miniApp/dist/app.js
+++ miniApp/dist/app.js
@@ -14,4 +14,20 @@ global.getAssetJson = function(path) {
     return json
 }
 
+// Load custom fonts
+global.loadCustomFonts = function() {
+    wx.loadFontFace({
+        global: true,
+        family: 'Satisfy-Regular',
+        source: 'url("/assets/fonts/Satisfy-Regular.ttf")',
+        success: function(res) {
+            console.log('[Font] Satisfy-Regular loaded successfully', res)
+        },
+        fail: function(err) {
+            console.warn('[Font] Satisfy-Regular load failed', err)
+        }
+    })
+}
+global.loadCustomFonts()
+
 render.initApp()

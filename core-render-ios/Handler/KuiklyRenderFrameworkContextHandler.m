d52d95422b44e5a670e66288845884cadb8c8ed5
HongShaojie <hsjcn@qq.com>
2026-08-06 17:26:00 +0800

    Fix(iOS) Bridge 参数丢失导致 fireViewEvent JSONException 崩溃

--- core-render-ios/Handler/KuiklyRenderFrameworkContextHandler.m
+++ core-render-ios/Handler/KuiklyRenderFrameworkContextHandler.m
@@ -101,7 +101,7 @@
     NSMutableArray * arguments = [[NSMutableArray alloc] initWithCapacity:6];
     for (id arg in args) {
         id ele = [KRConvertUtil nativeObjectToKotlinObject:arg];
-        [arguments addObject:ele ?: @""];
+        [arguments addObject:ele ?: [NSNull null]];
     }
     [_coreEntryInstance callKotlinMethodMethodId:(int32_t)method
                                             arg0:KRSafeArrayIndex(arguments, 0)

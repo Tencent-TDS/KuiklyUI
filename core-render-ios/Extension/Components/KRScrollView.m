0b1089029f329f75ca9a8e3b61642de3390e98a9
RoeKun <>
2026-09-09 20:25:20 +0800

    fix(ios): avoid scrollview insets abnormal value during bounce and deceleration

--- core-render-ios/Extension/Components/KRScrollView.m
+++ core-render-ios/Extension/Components/KRScrollView.m
@@ -783,6 +783,11 @@ KUIKLY_NESTEDSCROLL_PROTOCOL_PROPERTY_IMP
 }
 
 - (UIEdgeInsets)maxEdgeInsetsWithContentOffset:(CGPoint)contentOffset {
+    // 边界回弹、惯性滚动时一律不运行更新
+    if (self.isZoomBouncing || self.isDecelerating) {
+        return self.contentInset;
+    }
+    // 放行程序化更新、拖拽时、双指捏放时的Offset变化，并且让inset去响应
     if ([_css_directionRow boolValue]) {
         if (contentOffset.x < -self.contentInset.left) {
             return UIEdgeInsetsMake(self.contentInset.top, -contentOffset.x, self.contentInset.bottom, self.contentInset.right);
